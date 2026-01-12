4c1f684f0c6a4e6439949c9e177a0394155111b8
Parvej Ansari <116214778+lokeyparry@users.noreply.github.com>
2026-01-12 17:43:26 +0530

    j

--- server/server.js
+++ server/server.js
@@ -3,20 +3,21 @@ import cors from "cors"
 import "dotenv/config"
 import connectDB from "./config/mongodb.js"
 import { clerkMiddleware } from '@clerk/express'
+
 import clerkWebhooks from "./controllers/clerkWebhooks.js"
 
 const app = express()
 app.use(cors())
-await connectDB()
+connectDB()
     // middlewarre
-app.use(express.json)
+app.use(express.json())
 app.use(clerkMiddleware())
 
 // Api to listen clerk webhooks
 app.use("/api/clerk", clerkWebhooks)
 
 app.get('/', (req, res) => {
-    res.send("server connected successfully")
+    res.send("server connected successfullyyy")
 })
 
 const port = process.env.PORT || 3000
