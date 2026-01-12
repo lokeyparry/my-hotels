d77cd769876db4660e7f6140e847a3a416d914ff
Parvej Ansari <116214778+lokeyparry@users.noreply.github.com>
2026-01-12 17:04:40 +0530

    j

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -22,8 +22,8 @@ const clerkWebhooks = async(req, res) => {
                 {
                     const userData = {
                         _id: data.id,
-                        username: data.first_name + " " + data.last_name,
                         email: data.email_addresses[0].email_address,
+                        username: data.first_name + " " + data.last_name,
                         image: data.image_url,
                     };
                     await User.create(userData)
